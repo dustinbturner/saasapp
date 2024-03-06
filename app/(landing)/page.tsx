10a2929fe06f6d2aeb81452942c403fabfb029aa
Dustin Turner <dustin@dustinbturner.com>
2024-03-06 15:22:30 -0500

    adding auth

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function LandingPage() {
+  return (
+    <div>Landing Page (Unprotected)</div>
+  )
+}
